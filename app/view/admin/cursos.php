a056fa5d0cbcb08b4dabfbe8a10cd4a3a085455b
Christan RCSG <christian.floppy@gmail.com>
2021-04-27 12:15:02 -0500

    Estructiura
    Montamos la estructura inicial del proyecto
    
    Co-Authored-By: Jeniper70 <57198260+jennifer70@users.noreply.github.com>

---
+++ app/view/admin/cursos.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+
+<?php
+//Abrir para agregar los includes
+$tittle = "Cursos registrados";
+include("../includes/header.php");
+?>
+<body class="body-home">
+<div class="d-flex">
+    <?php include("../includes/navegation.php"); ?>
+    <div class="w-100">
+        <?php include("../includes/menu_admin.php"); ?>
+
+        <div id="content">
+            <!--Inicio contenido dinamico-->
+            <div class="componet-dinamico">
+                <h2>HOla</h2>
+                <h3>hola de Mostrando cursos</h3>
+            </div>
+            <!--FIN contenido dinamico-->
+            <?php include("../includes/footer.php");?>
+        </div>
+
+
+    </div>
+</div>
+<!-- script_js -->
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>SICEP</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid">
+    <form action="app/index.php">
+        <div class="form-group">
+            <label for="email">Email address:</label>
+            <input type="email" class="form-control" placeholder="Enter email" id="email">
+        </div>
+        <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" placeholder="Enter password" id="pwd">
+        </div>
+        <div class="form-group form-check">
+            <label class="form-check-label">
+                <input class="form-check-input" type="checkbox"> Remember me
+            </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+
+</body>
+</html>
