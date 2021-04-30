7962a9ea8b684e74dba7575938449afbb28ce39c
htmtmx <cesar.hpp96@hotmail.com>
2021-04-30 10:01:57 -0500

    BACK - Class Curso, Keywords, Grupo
    Composiciones:
    lista_temas
    lista_grupos
    lista_docs_solicitados
    lista_group_keys
    obj_profesor_autor
    obj_profesor_admin_acredita

---
+++ app/model/GRUPO.php
@@ -0,0 +1,76 @@
+<?php
+
+
+class GRUPO
+{
+private $id_grupo;
+private $id_curso_fk;
+private $grupo;
+private $estatus;
+
+    /**
+     * @return mixed
+     */
+    public function getIdGrupo()
+    {
+        return $this->id_grupo;
+    }
+
+    /**
+     * @param mixed $id_grupo
+     */
+    public function setIdGrupo($id_grupo): void
+    {
+        $this->id_grupo = $id_grupo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getIdCursoFk()
+    {
+        return $this->id_curso_fk;
+    }
+
+    /**
+     * @param mixed $id_curso_fk
+     */
+    public function setIdCursoFk($id_curso_fk): void
+    {
+        $this->id_curso_fk = $id_curso_fk;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGrupo()
+    {
+        return $this->grupo;
+    }
+
+    /**
+     * @param mixed $grupo
+     */
+    public function setGrupo($grupo): void
+    {
+        $this->grupo = $grupo;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEstatus()
+    {
+        return $this->estatus;
+    }
+
+    /**
+     * @param mixed $estatus
+     */
+    public function setEstatus($estatus): void
+    {
+        $this->estatus = $estatus;
+    }
+
+
+}
