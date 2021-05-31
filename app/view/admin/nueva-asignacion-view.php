66edaf78cb8da8066f5ab7d0adf8cea45f5dccd9
Jeniper70 <jeniper.666@outlook.com>
2021-05-31 14:52:14 -0500

    NUEVA ASIGNACION

--- app/view/admin/nueva-asignacion-view.php
+++ app/view/admin/nueva-asignacion-view.php
@@ -52,7 +52,7 @@
                                         <form class="user needs-validation" id="frm-add-asignacion" role="form" autocomplete="off" novalidate>                                    
                                             <div class="form-group row">
                                                 <div class="col-sm-2 mb-3 mb-sm-0">
-                                                    <label class="label" for="rbnProf">Profesor asignado</label>
+                                                    <label class="label" for="profesorAsig">Profesor asignado</label>
                                                 </div>
                                                 <div class="col-sm-10 mb-3 mb-sm-0">
                                                     <select class="form-control" id="profesorAsig">
@@ -74,7 +74,7 @@
                                             </div>                                            
                                             <div class="form-group row vertical-align-center">
                                                 <div class="col-sm-2 mb-3 mb-sm-0">
-                                                    <label class="label" for="rbnGrupo">Grupo: </label>
+                                                    <label class="label" for="grupo">Grupo: </label>
                                                 </div>
                                                 <div class="col-sm-4 mb-3 mb-sm-0 d-flex">
                                                     <!-- <input type="text" class="form-control form-control-user"  name="grupo" id="Grupo" required="" placeholder="Grupo"> -->
@@ -86,23 +86,20 @@
                                                     <img class="image mx-2 aling-center" src="https://img.icons8.com/flat-round/452/plus.png" width="30" height="30">
                                                 </div>
                                                 <div class="col-sm-2 mb-3 mb-sm-0">
-                                                    <label class="label" for="rbnModalidad">Modalidad: </label>
+                                                    <label class="label" for="modalidad">Modalidad: </label>
                                                 </div>
-                                                <div class="col-sm-3 mb-3 mb-sm-0">
+                                                <div class="col-sm-4 mb-3 mb-sm-0">
                                                     <!-- <input type="text" class="form-control form-control-user"  name="grupo" id="Grupo" required="" placeholder="Grupo"> -->
                                                     <select class="form-control" id="modalidad">
                                                         <option>Mixto</option>
                                                         <option>Presencial</option>
                                                         <option>A distancia</option>
                                                     </select>
-                                                </div>
-                                                <div class="col-sm-1 mb-3 mb-sm-0">
-                                                    <img src="/open-iconic/svg/plus.svg">
-                                                </div>                                                
+                                                </div>                                              
                                             </div>
                                             <div class="form-group row">
                                                 <div class="col-sm-2 mb-3 mb-sm-0">
-                                                    <label class="label" for="rbnGeneracion">Generación: </label>
+                                                    <label class="label" for="generacion">Generación: </label>
                                                 </div>
                                                 <div class="col-sm-4 mb-3 mb-sm-0">
                                                     <select class="form-control" id="generacion">
@@ -112,7 +109,7 @@
                                                     </select>
                                                 </div>
                                                 <div class="col-sm-2 mb-3 mb-sm-0">
-                                                    <label class="label" for="rbnSemestre">Semestre: </label>
+                                                    <label class="label" for="semestre">Semestre: </label>
                                                 </div>
                                                 <div class="col-sm-4 mb-3 mb-sm-0">
                                                     <select class="form-control" id="semestre">
@@ -124,132 +121,57 @@
                                             </div>
                                             <div class="form-group row">
                                                 <div class="col-sm-2 mb-3 mb-sm-0">
-                                                    <label class="label" for="rbnInicio">Inicio: </label>
-                                                </div>
-                                                <div class="col-sm-3 mb-3 mb-sm-0">
-                                                    <input id="datepicker" width="276" value="02/04/2018" />
-                                                </div>
-                                                <div class="col-sm-1 mb-3 mb-sm-0">
-                                                    <img src="/open-iconic/svg/plus.svg">
-                                                </div>
-                                                <div class="col-sm-2 mb-3 mb-sm-0">
-                                                    <label class="label" for="rbnFin">Fin: </label>
-                                                </div>
-                                                <div class="col-sm-3 mb-3 mb-sm-0">
-                                                    <select class="form-control" id="Fin">
-                                                        <option>Mixto</option>
-                                                        <option>Presencial</option>
-                                                        <option>A distancia</option>
-                                                    </select>
-                                                </div>
-                                                <div class="col-sm-1 mb-3 mb-sm-0">
-                                                    <img src="/open-iconic/svg/plus.svg">
-                                                </div>                                                
-                                            </div>
-                                            <div class="form-group row">
-                                                <div class="col-sm-2 mb-3 mb-sm-0">
-                                                    <label class="label" for="rbnLimite">Limite de inscripciones: </label>
+                                                    <label class="label" for="inicio">Inicio: </label>
                                                 </div>
                                                 <div class="col-sm-4 mb-3 mb-sm-0">
+                                                    <input type="date" name="inicio" max="3000-12-31" min="1000-01-01" class="form-control">
+                                                </div>
+                                                <div class="col-sm-2 mb-3 mb-sm-0">
+                                                    <label class="label" for="fin">Fin: </label>
+                                                </div>
+                                                <div class="col-sm-4 mb-3 mb-sm-0">
+                                                <input type="date" name="fin" max="3000-12-31" min="1000-01-01" class="form-control">
+                                                </div>                                              
+                                            </div>
+                                            <div class="form-group row">
+                                                <div class="col-sm-3 mb-3 mb-sm-0">
+                                                    <label class="label" for="dateLimite">Limite de inscripciones: </label>
+                                                </div>
+                                                <div class="col-sm-3 mb-3 mb-sm-0">
                                                     <!-- <input type="text" class="form-control form-control-user"  name="grupo" id="Grupo" required="" placeholder="Grupo"> -->
-                                                    <input type="date" name="dateLimInsc" max="3000-12-31" min="2021-05-25" class="form-control">
+                                                    <input type="date" name="dateLimite" max="3000-12-31" min="2021-05-25" class="form-control">
                                                 </div>
                                                 <div class="col-sm-2 mb-3 mb-sm-0">
-                                                    <label class="label" for="rbnFin">Fin: </label>
-                                                </div>
-                                                <div class="col-sm-4 mb-3 mb-sm-0">
-                                                <input type="date" name="bday" max="3000-12-31" min="1000-01-01" class="form-control">
-                                                </div>                                                
-                                            </div>
-                                            <!-- <div class="form-group">
-                                                <input type="email" class="form-control form-control-user"  name="correoAlumno" id="correoAlumno" required="" placeholder="Correo Electrónico">
-                                            </div>
-                                            <div class="form-group row">
-                                                <div class="col-sm-6 mb-3 mb-sm-0">
-                                                    <input type="password" class="form-control form-control-user" name="contrasena"  id="contrasena" required="" placeholder="Password">
-                                                </div>
-                                                <div class="col-sm-6">
-                                                    <input type="password" class="form-control form-control-user" name="contrasenaconfirmar" id="contrasenaconfirmar" required="" placeholder="Repeat Password">
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="form-group row">
-                                                <div class="col-sm-4 mb-3 mb-sm-0">
-                                                    <label for="exampleFormControlSelect2">Estado</label>
-                                                </div>
-                                                <div class="col-sm-8">
-                                                    <select class="form-control" name="estados"  id="estados">
-                                                    </select>
-                                                </div>
-                                            </div> -->                                            
-                                            <div class="form-group row">
-                                                <div class="col-sm-4 mb-3 mb-sm-0">
-                                                    <label for="exampleFormControlSelect2">Municipio</label>
-                                                </div>
-                                                <div class="col-sm-8">
-                                                    <select class="form-control" name="municipios"  id="municipios">                                                        
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="form-group row">
-                                                <div class="col-sm-4 mb-3 mb-sm-0">
-                                                    <label for="exampleFormControlSelect2">Procedencia</label>
-                                                </div>
-                                                <div class="col-sm-8">
-                                                    <select class="form-control" name="procedencia"  id="procedencia">
-                                                        <option value="1">Comunidad FESC</option>
-                                                        <option value="2">Comunidad UNAM</option>
-                                                        <option value="3">Ex-Alumno</option>
-                                                        <option value="4">Comunidad Externa</option>
-                                                        <option value="0">Otro</option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="form-group row">
-                                                <div class="col-sm-4 mb-3 mb-sm-0">
-                                                    <label for="exampleFormControlSelect2">Universidad</label>
-                                                </div>
-                                                <div class="col-sm-8">
-                                                    <select class="form-control" name="universidades"  id="universidades">
-                                                        <!--AJAX result DB -->
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="form-group row">
-                                                <div class="col-sm-4 mb-3 mb-sm-0">
-                                                </div>
-                                                <div class="col-sm-8">
-                                                    <input type="text" class="form-control form-control-user d-none" name="nombreUni"  id="nombreUni" placeholder="">
-                                                </div>
-                                            </div>
-                                            <div class="form-group row">
-                                                <div class="col-sm-6 mb-3 mb-sm-0">
-                                                    <input type="text" class="form-control form-control-user" name="carrera"  id="carrera" required="" placeholder="Carrera/Profesion actual">
-
-                                                </div>
-                                                <div class="col-sm-6">
-                                                    <input type="text" class="form-control form-control-user" name="matricula"  id="matricula" required="" placeholder="No de Cuenta/Matricula">
-                                                </div>
-                                                <div class="col-sm-6">
-                                                    <input type="hidden" class="form-control form-control-user" name="catpcha"  id="catpcha" required="" placeholder="No de Cuenta/Matricula">
-                                                </div>
-                                            </div>
-                                            <hr>
-                                            <div class="form-group row">
-                                                <div class="col-sm-8 mb-3 mb-sm-0">
-                                                    <p>Al hacer clic en "Registrarme", aceptas nuestras Condiciones,
-                                                        la Política de datos y la Política de cookies.</p>
+                                                    <label class="label" for="numCupo">Cupo: </label>
                                                 </div>
                                                 <div class="col-sm-4">
-                                                    <input type="submit" id="btnEnviar" name="btnEnviar" value="Registrarme" class="btn btn-primary btn-user btn-block">
+                                                    <input class="form-control" type="number" value="150" id="numCupo">
                                                 </div>
                                             </div>
-                                            <div id="formError" class="alert alert-danger d-none" role="alert">
-                                                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-                                                <span id="Mensaje">Error: </span>
+                                            <hr>
+                                            <div class="form-group row">
+                                                <div class="col-sm-2 mb-3 mb-sm-0">
+                                                    <label>Habilitar inscripción:</label>
+                                                </div>
+                                                <div class="col-sm-4 mb-3 mb-sm-0">                               
+                                                    <label>Aplicar descuento*</label>
+                                                </div>
+                                                <div class="col-sm-2 mb-3 mb-sm-0">                               
+                                                    <label>Notas:</label>
+                                                </div>
+                                                <div class="col-sm-4 mb-3 mb-sm-0">                               
+                                                    <label>text</label>
+                                                </div>
                                             </div>
-                                            <div id="summary"></div> <!-- Espacio donde se colocara un mensaje general para el usuario -->
+                                            <div class="form-group row">
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                                                <label class="form-check-label" for="defaultCheck1">
+                                                    Default checkbox
+                                                </label>
+                                            </div>                                           
+                                            </div>
+
                                         </form>
                                         <!-- termina el formulario-->
                                     </div>
