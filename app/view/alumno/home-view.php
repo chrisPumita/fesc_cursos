31b2b59ded4b7f52b212cfb166b997ceebd55e6e
Christan RCSG <christian.floppy@gmail.com>
2021-06-05 19:22:21 -0500

    BK - Control View Rol
    Creacion de archivos de Control de Rol para las vistas

--- app/view/alumno/home-view.php
+++ app/view/alumno/home-view.php
@@ -4,14 +4,14 @@
 <?php
 //Abrir para agregar los includes
 $tittle = "Bienvenido al panel del Alumno";
-include("../includes/header.php");
+include("./view/includes/header.php");
 ?>
 <body class="body-home">
 <div class="d-flex">
-    <?php include("../includes/alumno-menupc.php"); ?>
+    <?php include("./view/includes/alumno-menupc.php"); ?>
     
     <div class="w-100">
-        <?php include("../includes/alumno-menutel.php"); ?>
+        <?php include("./view/includes/alumno-menutel.php"); ?>
         
         <div id="content">
             <!--Inicio contenido dinamico-->
@@ -19,7 +19,7 @@ include("../includes/header.php");
                 <?php include ("alumno-inicio-view.php")?>
             </div>
             <!--FIN contenido dinamico-->
-            <?php include("../includes/footer.php");?>
+            <?php include("./view/includes/footer.php");?>
         </div>
     </div>
 </div>
--- app/view/profesor/home-view.php
+++ app/view/profesor/home-view.php
@@ -3,24 +3,24 @@
 
 <?php
 //Abrir para agregar los includes
-$tittle = "Bienvenido al panel de administrador";
-include("../includes/header.php");
+$tittle = "Bienvenido al panel del Alumno";
+include("./view/includes/header.php");
 ?>
 <body class="body-home">
 <div class="d-flex">
-    <?php include("../includes/navegation.php"); ?>
+    <?php include("./view/includes/alumno-menupc.php"); ?>
+
     <div class="w-100">
-        <?php include("../includes/menu_admin.php"); ?>
+        <?php include("./view/includes/alumno-menutel.php"); ?>
 
         <div id="content">
             <!--Inicio contenido dinamico-->
-            <div class="componet-dinamico">
-                <h1>PAGINA DE INICIO DE PROFESOR</h1>
+            <div class="componet-dinamico bg-grey">
+               <h1>Inlude files profesor</h1>
             </div>
             <!--FIN contenido dinamico-->
-            <?php include("../includes/footer.php");?>
+            <?php include("./view/includes/footer.php");?>
         </div>
-
     </div>
 </div>
 <!-- script_js -->
